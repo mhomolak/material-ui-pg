7892b7d19445a39b182240f9ab2fa2067a843a2f
Mike <Mhomolak@yahoo.com>
2016-09-21 13:29:02 -0600

    initial commit and refactor of architecture implementation

--- demo/demo.js
+++ demo/demo.js
@@ -1,23 +1,17 @@
-import MyComponent from '../main'; // to demo direct API usage
-
-// When available on npm, consumer usage would be similar to:
-// import MyComponent from '@pearson-components/[component-name]'
+import MyComponent from '../main';
 
 function init() {
 
   // Demo eventing API
   document.body.dispatchEvent(new CustomEvent('o.InitMyComponent', {
     detail: {
-      elementId: 'demo-target1',
-      greeting: 'Hello world!'
+      elementId: 'demo-target1'
     }
   }));
 
   // Demo direct API
   new MyComponent({
-    elementId: 'demo-target2',
-    greeting: 'Bonjour le monde!',
-    locale: 'fr'
+    elementId: 'demo-target2'
   });
 
 }
